d70479e8c8ea8882aa8e031b3c2e9838eb182219
mr-sazzad <main.sazza@gmail.com>
2023-10-18 12:54:40 +0600

    user blogs part commited

---
+++ app/(site)/admin/dashboard/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Loading from "@/app/loading";
+import { useGetAllBlogsQuery } from "@/app/redux/api/blogs/blogApi";
+import { useGetAllFeedbacksQuery } from "@/app/redux/api/feedback/feedbackApi";
+import { useGetAllServicesQuery } from "@/app/redux/api/services/serviceApi";
+import { useGetAllUserQuery } from "@/app/redux/api/userApi";
+
+const AdminDashboard = () => {
+  const { data: users, isLoading: usersLoading } =
+    useGetAllUserQuery(undefined);
+  const { data: services, isLoading: servicesLoading } =
+    useGetAllServicesQuery(undefined);
+
+  const { data: blogs } = useGetAllBlogsQuery(undefined);
+  const { data: feedbacks } = useGetAllFeedbacksQuery(undefined);
+
+  if (usersLoading || servicesLoading) {
+    <Loading />;
+  }
+
+  const totalUsers = users?.length - 1;
+  const totalServices = services?.length - 1;
+
+  const totalBlogs = blogs?.length - 1;
+  const totalFeedbacks = feedbacks?.length - 1;
+
+  return (
+    <div className="container mx-auto">
+      <div className="mx-4 md:mx-8 lg:mx-10">
+        <div className="flex flex-col lg:flex-row justify-between items-center my-5 gap-3 w-full">
+          <div className="flex flex-col lg:flex-row gap-3 w-full">
+            <div className="w-full h-[100px] bg-green-300 p-5 rounded-md hover:bg-green-400 transition">
+              <h2 className="text-2xl font-medium">Total Active Users</h2>
+              <p className="text-3xl font-bold">
+                {totalUsers >= 9
+                  ? `${totalUsers}`
+                  : totalUsers?.toString().padStart(2, "0")}
+              </p>
+            </div>
+            <div className="w-full h-[100px] bg-indigo-300 p-5 rounded-md hover:bg-indigo-400 transition">
+              <h2 className="text-2xl font-medium">Total Services</h2>
+              <p className="text-3xl font-bold">
+                {totalServices >= 9
+                  ? `${totalServices}`
+                  : totalServices?.toString().padStart(2, "0")}
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex-row gap-3 w-full">
+            <div className="w-full h-[100px] bg-green-300 p-5 rounded-md hover:bg-green-400 transition">
+              <h2 className="text-2xl font-medium">We Got Feedbacks</h2>
+              <p className="text-3xl font-bold">
+                {" "}
+                {totalFeedbacks >= 9
+                  ? `${totalFeedbacks}`
+                  : totalFeedbacks?.toString().padStart(2, "0")}
+              </p>
+            </div>
+            <div className="w-full h-[100px] bg-indigo-300 p-5 rounded-md hover:bg-indigo-400 transition">
+              <h2 className="text-2xl font-medium">Total Blogs</h2>
+              <p className="text-3xl font-bold">
+                {totalBlogs >= 9
+                  ? `${totalBlogs}`
+                  : totalBlogs?.toString().padStart(2, "0")}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;

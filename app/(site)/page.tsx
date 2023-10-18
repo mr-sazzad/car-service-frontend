81dff71cb611ec947ce6f06358c26fd7599622a3
mr-sazzad <main.sazza@gmail.com>
2023-10-18 13:22:52 +0600

    page.tsx file commited

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,23 @@
+import AvailableServices from "@/components/AvailableServices";
+import LatestNews from "@/components/LatestNews";
+import UpcomingServices from "@/components/UpcomingServices";
+import Events from "@/components/events/Events";
+import ClientReview from "@/components/reviews/ClientReview";
+import Survey from "@/components/survey/Survey";
+import OurTeam from "@/components/team/OurTeam";
 import Header from "../../components/Header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <AvailableServices />
+      <UpcomingServices />
+      <Events />
+      <Survey />
+      <ClientReview />
+      <LatestNews />
+      <OurTeam />
     </div>
   );
 }
